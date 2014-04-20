b0dd5a7bb9dbda6fda03641c2a72137f5ebff9af
SRCres <srcres@gmail.com>
2014-04-19 21:41:22 -0300

    Camino del cometa con líneas punteadas.

--- dist/comet.js
+++ dist/comet.js
@@ -53918,26 +53918,50 @@ define('js/views/Comet',[
 ], function($, _, Backbone, THREE, appConfig, App) {
   var CometView = Backbone.View.extend({
     initialize: function() {
-      var material = new THREE.MeshLambertMaterial({
+      var sphereMaterial = new THREE.MeshLambertMaterial({
             color: this.model.get('color')
           }),
           radius = this.model.get('radius'),
           position = this.model.get('position'),
-          // Reemplazar el coeficiente de cantidad de segmentos por la de la configuración de calidad.
           sphereGeometry = new THREE.SphereGeometry(radius, appConfig.render.sphere.width_segments, appConfig.render.sphere.height_segments);
       
-      this.sphere = new THREE.Mesh(sphereGeometry, material);
+      this.sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
       this.sphere.position = new THREE.Vector3(position.x, position.y, 0);
-
       App.scene.add(this.sphere);
 
+      var pathMaterial = new THREE.LineDashedMaterial({
+            color: 0xFFFFFF,
+            dashSize: 10,
+            gapSize: 10
+          }),
+          pathGeometry = new THREE.Geometry();
+
+      this.vertices = [];
+      for (var i = 0; i < 100; i++) {
+        this.vertices.push(this.sphere.position.clone());
+      }
+      pathGeometry.vertices = this.vertices;
+
+      this.path = new THREE.Line(pathGeometry, pathMaterial);
+      App.scene.add(this.path);
+
       this.listenTo(this.model, 'change', this.render);
+    },
+
+    renderPath: function() {
+      this.vertices.pop();
+      this.vertices.unshift(this.sphere.position.clone());
+      this.path.geometry.vertices = this.vertices;
+      this.path.geometry.computeLineDistances();
+      this.path.geometry.verticesNeedUpdate = true;
+      this.path.geometry.lineDistancesNeedUpdate = true;
     },
 
     render: function() {
       var position = this.model.get('position');
       this.sphere.position.x = position.x;
       this.sphere.position.y = position.y;
+      this.renderPath();
     }
   });
 
